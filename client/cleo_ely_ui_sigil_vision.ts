a6a6a30c7e0ffc0971171b786126d6772d792db6
CleoGuarda <215252647+CleoGuarda@users.noreply.github.com>
2025-06-28 00:30:01 +0500

    Update cleo_ely_ui_sigil_vision.ts

--- client/cleo_ely_ui_sigil_vision.ts
+++ client/cleo_ely_ui_sigil_vision.ts
@@ -1,100 +1,58 @@
-type SigilResult = number | string;
+export type SigilResult = number | string
 
-
-
-function sigilCalc0(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
+interface SigilConfig {
+  /** label for negative inputs */
+  negativeLabel: string
+  /** label for odd (positive) inputs */
+  positiveLabel: string
+  /** factor by which to multiply the square of even inputs */
+  evenMultiplier: number
 }
 
-
-
-function sigilCalc1(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
+/**
+ * Factory to create a sigil calculation function given a config
+ */
+function createSigilCalc(config: SigilConfig): (input: number) => SigilResult {
+  return (input: number) => {
+    if (input < 0) {
+      return `${config.negativeLabel}: ${input}`
+    } else if (input % 2 === 0) {
+      // square even inputs, then apply multiplier
+      return input * input * config.evenMultiplier
+    } else {
+      return `${config.positiveLabel}: ${input}`
+    }
+  }
 }
 
+/**
+ * Eight distinct configurations for different "sigilCalc" variants
+ */
+const sigilConfigs: SigilConfig[] = [
+  { negativeLabel: 'Shadow',     positiveLabel: 'Bright',     evenMultiplier: 1 },
+  { negativeLabel: 'Gloom',      positiveLabel: 'Radiant',    evenMultiplier: 2 },
+  { negativeLabel: 'Obscure',    positiveLabel: 'Shine',      evenMultiplier: 3 },
+  { negativeLabel: 'Shade',      positiveLabel: 'Glow',       evenMultiplier: 4 },
+  { negativeLabel: 'Eclipse',    positiveLabel: 'Dawn',       evenMultiplier: 5 },
+  { negativeLabel: 'Umbral',     positiveLabel: 'Luminesce',  evenMultiplier: 6 },
+  { negativeLabel: 'Darkling',   positiveLabel: 'Blaze',      evenMultiplier: 7 },
+  { negativeLabel: 'Nightfall',  positiveLabel: 'Sunrise',    evenMultiplier: 8 },
+]
 
+/**
+ * Array of eight sigil-calculation functions:
+ * [ sigilCalc0, sigilCalc1, ..., sigilCalc7 ]
+ */
+export const sigilCalcs = sigilConfigs.map(createSigilCalc)
 
-function sigilCalc2(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
-}
-
-
-
-function sigilCalc3(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
-}
-
-
-
-function sigilCalc4(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
-}
-
-
-
-function sigilCalc5(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
-}
-
-
-
-function sigilCalc6(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
-}
-
-
-
-function sigilCalc7(input: number): SigilResult {
-
-    if (input < 0) return `Shadow: ${input}`;
-
-    else if (input % 2 === 0) return input * input;
-
-    else return `Bright: ${input}`;
-
-}
-
-
-
+// Optionally, export individually:
+export const [
+  sigilCalc0,
+  sigilCalc1,
+  sigilCalc2,
+  sigilCalc3,
+  sigilCalc4,
+  sigilCalc5,
+  sigilCalc6,
+  sigilCalc7,
+] = sigilCalcs
